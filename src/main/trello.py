caefe57219e02fe01f9a9541bf22f52674c4166e
updraft <updraft.1970@gmail.com>
2021-05-07 08:16:40 +0900

    feat:trello class methed init create

--- src/main/trello.py
+++ src/main/trello.py
@@ -1,2 +1,35 @@
 # TODO class trello
 
+class TrelloAPI():
+    """
+    docstring
+    """
+
+    def __init__(self):
+        """
+        docstring
+        """
+        client = None
+        api_key = None
+        token = None
+
+    def _TrelloClientAccess(self):
+        """
+        docstring
+        """
+        pass
+
+    def _GetApiKeyFile(self, path="../../mine/Token.txt"):
+        """
+        docstring
+        """
+        with open(path, encoding="utf-8") as target:
+            result = target.readlines()
+        result = list(map(lambda i: i.rstrip("\n"), result))
+        self.api_key, self.token = result
+
+
+if __name__ == "__main__":
+    temp = [1, 2]
+    a, b = temp
+    print(a, b)
